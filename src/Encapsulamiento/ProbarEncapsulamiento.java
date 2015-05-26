949325c6d82d128711e8491c7cb1df114ebcc595
T-107 <T-107@PC254>
2015-05-25 19:46:45 -0500

    clase 25 de mayo

---
+++ src/Encapsulamiento/ProbarEncapsulamiento.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulamiento;
+
+
+public class ProbarEncapsulamiento {
+
+   
+    public static void main(String[] args) {
+       Usuario u1=new Usuario();
+       u1.setEdad(22);
+        System.out.println("Tu edad es:"u1.getEdad());
+    }
+    
+}
---
+++ src/Encapsulamiento/Usuario.java
@@ -0,0 +1,20 @@
+
+package Encapsulamiento;
+
+
+public class Usuario {
+private String nombre;
+private int edad;
+public void setNombre(String nombre){
+this.nombre=nombre;
+}
+public void setEdad(int edad){
+    this.edad=edad; 
+}
+public String getNombre(){
+    return nombre; 
+}
+public int getedad(){
+    return edad;
+}
+}
