058b0e18b74ace9d7d2de97dec5329410f124eeb
T-107 <T-107@PC249>
2015-06-15 19:43:31 -0500

    clase de hoy 15

---
+++ src/miprimeraexception/menordeedadexception.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraexception;
+
+/**
+ *
+ * @author T-107
+ */
+public class menordeedadexception extends Exception{
+public menordeedadexception(){
+   super("Eres un chamaco pervertido,le dire a tu mama"+"registar");
+   
+           
+    
+}
+    
+    
+}
